e983f1b505ed994b6bd4b5af57c5708ed0fb2a03
chetansankpal7007@gmail.com <chetansankpal7007@gmail.com>
2023-10-04 08:38:10 +0530

    login

---
+++ src/components/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import './Login.css';
+export default function Login() {
+    const [username, setuserName] =useState ();
+    const [password, setPassword] =useState ();
+    const LOGINURL = 'http://localhost:8090/emp/emp-login';
+
+
+    const checkUserLogin = async() => {
+        let playload = {
+            email: username,
+            password: password
+        }
+        let res = await fetch(LOGINURL, {
+            method: "POST",
+            body: JSON.stringify(playload),
+            headers: {
+                        'Content-type': 'application/json; charset=UTF-8',
+                    }
+        })
+        if(res.status == 200) {
+            let user = await res.json();
+            console.log(user[0]._id)
+            localStorage.setItem('_id', user[0]._id);
+            localStorage.setItem('emp_id', user[0].emp_id);
+            localStorage.setItem('emp_name', user[0].emp_name);
+            alert("login");
+
+        } else {
+            alert("Please check user name and password");
+        }
+
+    }
+
+
+  return (
+        <div>
+            <form  >
+                <label htmlFor="uname">User Name</label>
+                <input type="text" value={username} onChange={e => {setuserName(e.target.value)}} id="uname" name="firstname" placeholder="Your User Name.."/>
+
+                <label htmlFor="pass">password</label>
+                <input type="text" value={password} id="lname" onChange={e=> {setPassword(e.target.value)}} name="pass" placeholder="Your password.."/>
+            
+                <input type="button" value="Submit" onClick={checkUserLogin} />
+            </form>
+    </div>
+  )
+}
