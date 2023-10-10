e53d4fcd3f9f3f98e754b1b839a4eb412de022e9
chetansankpal7007@gmail.com <chetansankpal7007@gmail.com>
2023-10-10 08:32:57 +0530

    emp list and product add

--- src/components/Emp/Emplist.js
+++ src/components/Emp/Emplist.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './Emp.css'
 
 export default function Emplist() {
+
+  const [empList, setlist] = useState([]);
+  const URL = 'http://localhost:8090/emp/get-emp'
+
+  useEffect(()=>{
+    getEmpList();
+  }, []);
+
+  const getEmpList = async () => {
+    let res = await fetch(URL);
+    let data =await res.json();
+    setlist(data);
+    console.log(data.length);
+  }
+   
+
+
   return (
     <div>
       <h1>Emp List</h1>
+      <table id="customers">
+      <thead>
+        <tr>
+          <th>Emp Id</th>
+          <th>Name</th>
+          <th>Email Id</th>
+          <th>Contact no</th>
+          <th>Role</th>
+        </tr>
+      </thead>
+        <tbody>
+        {
+          empList?.map((emp)=>{
+          return (<tr  key={emp.emp_id}>
+            <td>{emp.emp_id}</td>
+            <td>{emp.emp_name}</td>
+            <td>{emp.emp_email}</td>
+            <td>{emp.emp_contact}</td>
+            <td>{emp.emp_role}</td>
+          </tr>)
+          })
+        }
+        </tbody>
+        
+      </table>
     </div>
   )
 }
--- src/components/Product/Productadd.js
+++ src/components/Product/Productadd.js
@@ -1,9 +1,133 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './product.css'
 
 export default function Productadd() {
+  const ADDAPI = "http://localhost:8090/product/add-product";
+
+  const [proid, setproId] = useState('');
+  const [proname, setproName] = useState('');
+  const [proprice, setproPrice] = useState('');
+  const [prodiscount, setproDiscount] = useState('');
+  const [prodesc, setproDesc] = useState('');
+  const [isadd, setisAdd] = useState(false);
+
+  const addPro = async () => {
+    setisAdd(true);
+    if(proid.length> 0 && proname.length>0 && proprice.length>0 && prodiscount.length>0 && prodesc.length>0){
+    let playLoad = {
+      p_id: proid,
+      p_name: proname,
+      p_price: proprice,
+      p_disc: prodiscount,
+      p_desc: prodesc,
+    };
+    console.log(playLoad);
+    let res = await fetch(ADDAPI, {
+      method: "POST",
+      body: JSON.stringify(playLoad),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    // let resJson = await res.json();
+    // alert("Employee add");
+    // console.log(resJson);
+    if(res.status == 200){
+      alert("Product added")
+      setproId("");
+    setproName("");
+    setproPrice("");
+    setproDiscount("");
+    setproDesc("");
+    setisAdd(false);
+    }else {
+      alert("Wait");
+    }
+
+    
+
+  } else {        
+    alert("Please enter data");
+  }
+}
   return (
-    <div>
-       <h1>Product Added</h1>
+    <div className="container">
+      <div className="text">Product Add</div>
+      <form>
+        <div className="form-row">
+          <div className="input-data">
+            <input
+              type="number"
+              onChange={(e) => setproId(e.target.value)}
+              value={proid}
+              required
+            />
+            {proid?.length == 0 && isadd ?  <span>Please Enter Emp Id<br/></span> : ''  } <br/>
+          
+            <div className="underline"></div>
+            <label htmlFor="">Pro Id</label>
+          </div>
+          <div className="input-data">
+            <input
+              type="text"
+              onChange={(e) => setproName(e.target.value)}
+              value={proname}
+              required
+            />
+            {proname?.length == 0 && isadd ?  <span>Please Enter Emp Name<br/></span> : ''  } <br/>
+          
+            <div className="underline"></div>
+            <label htmlFor="">Product Name</label>
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="input-data">
+            <input
+              type="text"
+              onChange={(e) => setproDiscount(e.target.value)}
+              value={prodiscount}
+              required
+            />
+            {prodiscount?.length == 0 && isadd ?  <span>Please Enter Emp Email<br/></span> : ''  } <br/>
+          
+            <div className="underline"></div>
+            <label htmlFor="">Product Discount</label>
+          </div>
+          <div className="input-data">
+            <input
+              type="number"
+              onChange={(e) => setproPrice(e.target.value)}
+              value={proprice}
+              required
+            />
+            {proprice?.length == 0 && isadd ?  <span>Please Enter Emp Contact<br/></span> : ''  } <br/>
+          
+            <div className="underline"></div>
+            <label htmlFor="">Product Price</label>
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="input-data">
+            <input
+              type="text"
+              onChange={(e) => setproDesc(e.target.value)}
+              value={prodesc}
+              required
+            />
+            {prodesc?.length == 0 && isadd ?  <span>Please Enter Emp Password<br/></span> : ''  } <br/>
+          
+            <div className="underline"></div>
+            <label htmlFor="">Product Desc</label>
+          </div>
+        </div>
+      
+        <div className="form-row submit-btn">
+          <div className="input-data">
+            <div className="inner"></div>
+            <input type="button" onClick={addPro} value="submit" />
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
